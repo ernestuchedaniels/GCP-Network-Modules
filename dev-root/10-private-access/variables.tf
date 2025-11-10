e4aff88fb15f2b1ee7ffb70c86bc401970d775f5
Ernest Daniels <ernestuchedaniels@gmail.com>
2025-11-10 11:20:20 -0500

    Simplify PSC configuration - auto-derive region from subnet
    - Remove region from user input variables
    - Auto-derive region from subnet self-link
    - Users only need app_name and service_attachment_uri
    - Ultra-simple developer interface

--- dev-root/10-private-access/variables.tf
+++ dev-root/10-private-access/variables.tf
@@ -1,12 +1,8 @@
 variable "psc_endpoints" {
-  description = "Map of PSC endpoints to create"
+  description = "Map of PSC endpoints to create for Google services"
   type = map(object({
-    name                   = string
     app_name              = string
-    #service_attachment_uri = string
-    region                = string
-    description           = string
-    labels                = map(string)
+    service_attachment_uri = string
   }))
 }
 
