407fe19425fce2ee68713622f7cb772e4b43a087
Ernest Daniels <ernestuchedaniels@gmail.com>
2025-11-10 10:38:53 -0500

    Fix PSC outputs to use correct module references
    - Reference psc_endpoints module (not psc_endpoint)
    - Remove non-existent endpoint_name attribute
    - Only output available psc_internal_ip from module

--- dev-root/10-private-access/outputs.tf
+++ dev-root/10-private-access/outputs.tf
@@ -1,9 +1,8 @@
-output "psc_endpoint_ip" {
-  description = "IP address of the PSC endpoint"
-  value       = module.psc_endpoint.psc_internal_ip
-}
-
-output "peering_state" {
-  description = "State of the VPC peering connection"
-  value       = module.vpc_peering.peering_state
+output "psc_endpoints" {
+  description = "Map of created PSC endpoints with IPs"
+  value = {
+    for k, v in module.psc_endpoints : k => {
+      ip = v.psc_internal_ip
+    }
+  }
 }
