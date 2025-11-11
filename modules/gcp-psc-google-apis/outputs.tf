10297e41ba4afcee6f90d8f5cb668331e3ee0079
Ernest Daniels <ernestuchedaniels@gmail.com>
2025-11-11 10:46:22 -0500

    Remove global address resource from PSC module

--- modules/gcp-psc-google-apis/outputs.tf
+++ modules/gcp-psc-google-apis/outputs.tf
@@ -1,6 +1,6 @@
 output "psc_endpoint_ip" {
   description = "IP address of the PSC endpoint"
-  value       = google_compute_global_address.psc_address.address
+  value       = google_compute_global_forwarding_rule.psc_endpoint.ip_address
 }
 
 output "psc_endpoint_name" {
