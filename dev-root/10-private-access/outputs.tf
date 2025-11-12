2f0fdd482b2b46629717694262998ddb3c3fdd8a
Ernest Daniels <ernestuchedaniels@gmail.com>
2025-11-12 09:05:18 -0500

    Add regional Google API PSC module with auto-allocated IPs and global access

--- dev-root/10-private-access/outputs.tf
+++ dev-root/10-private-access/outputs.tf
@@ -1,7 +1,17 @@
 output "google_api_psc_endpoints" {
-  description = "Google API PSC endpoint details"
+  description = "Google API PSC endpoint details (global)"
   value = {
     for k, v in module.google_api_psc : k => {
+      ip                 = v.psc_endpoint_ip
+      forwarding_rule_id = v.forwarding_rule_id
+    }
+  }
+}
+
+output "google_api_psc_regional_endpoints" {
+  description = "Google API PSC endpoint details (regional)"
+  value = {
+    for k, v in module.google_api_psc_regional : k => {
       ip                 = v.psc_endpoint_ip
       forwarding_rule_id = v.forwarding_rule_id
     }
--- dev-root/10-private-access/terraform.tfvars
+++ dev-root/10-private-access/terraform.tfvars
@@ -5,6 +5,17 @@ psc_google_apis = {
     ip_address = "10.10.100.1"
   }
 }
+
+# Google API PSC Endpoints (regional)
+psc_google_apis_regional = {
+  storage = {
+    target        = "storage.us-west1.rep.googleapis.com"
+    region        = "us-west1"
+    app_name      = "cache"
+    global_access = true
+  }
+}
+
 # Third-party PSC Service Attachments (regional)
 psc_service_attachments = {
   # Example: Uncomment and configure when you have a producer service
