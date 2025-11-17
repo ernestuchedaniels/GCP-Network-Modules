4cc1c1f9a7d65466bec8908f7ada5c762d17b5b2
Ernest Daniels <ernestuchedaniels@gmail.com>
2025-11-17 10:22:16 -0500

    Enable HA VPN for dev-shared-vpc

--- dev-root/08-hybrid-connectivity/variables.tf
+++ dev-root/08-hybrid-connectivity/variables.tf
@@ -1,20 +1,56 @@
 variable "region" {
-  description = "GCP region"
+  description = "GCP region for Cloud Routers"
   type        = string
 }
 
-variable "asn_number" {
-  description = "BGP ASN number"
-  type        = number
+variable "enable_vpn" {
+  description = "Enable HA VPN"
+  type        = bool
+  default     = false
 }
 
-variable "vlan_attachments" {
-  description = "Map of VLAN attachments to create"
-  type = map(object({
-    name        = string
-    region      = string
-    type        = string
-    bandwidth   = string
+variable "vpn_region" {
+  description = "GCP region for VPN resources"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "peer_vpn_gateway_id" {
+  description = "Peer VPN gateway ID"
+  type        = string
+  default     = ""
+}
+
+variable "vpn_shared_secrets" {
+  description = "Shared secrets for VPN tunnels"
+  type        = list(string)
+  default     = []
+  sensitive   = true
+}
+
+variable "vpn_interface_ip_ranges" {
+  description = "IP ranges for VPN router interfaces"
+  type        = list(string)
+  default     = []
+}
+
+variable "vpn_peer_ip_addresses" {
+  description = "Peer IP addresses for BGP"
+  type        = list(string)
+  default     = []
+}
+
+variable "vpn_peer_asn" {
+  description = "Peer ASN for BGP"
+  type        = number
+  default     = 65000
+}
+
+variable "vpn_advertised_ip_ranges" {
+  description = "IP ranges to advertise via BGP"
+  type = list(object({
+    range       = string
     description = string
   }))
+  default = []
 }
--- dev-root/09-service-projects/terraform.tfvars
+++ dev-root/09-service-projects/terraform.tfvars
@@ -1,6 +1,12 @@
 service_projects = {
   app_project = {
-    project_id = "dev-app-project"
-    name       = "Dev Application Project"
+    project_id      = "visa-dev-app-001"
+    project_name    = "Dev Application Project"
+    billing_account = "01C3E8-4E0F3A-B5E5D1"
+    org_id          = "339993026748"
+    labels = {
+      environment = "dev"
+      team        = "application"
+    }
   }
 }
